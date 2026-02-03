947ee7ccb2b98b795253041f12912e6e649135ce
Thomas Dillmann <info@its-dillmann.de>
2026-02-03 19:49:34 +0100

    Initial Commit

---
+++ Train with Me/Train_with_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Train_with_MeApp.swift
+//  Train with Me
+//
+//  Created by Thomas on 03.02.26.
+//
+
+import SwiftUI
+
+@main
+struct Train_with_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Train with MeTests/Train_with_MeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Train_with_MeTests.swift
+//  Train with MeTests
+//
+//  Created by Thomas on 03.02.26.
+//
+
+import Testing
+@testable import Train_with_Me
+
+struct Train_with_MeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
